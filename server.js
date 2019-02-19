14e0d3cbc2707c8351fa3334568491834ff600af
James Gary <gmarchesi888@gmail.com>
2019-02-19 15:40:35 -0500

    POST functionality working.

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const MongoClient = require("mongodb").MongoClient;
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT;
 
-mongoose.Promise = global.Promise;
+let db;
 
-let db; 
-
-mongoose
-  .connect(process.env.DB, { useNewUrlParser: true })
-  .then(console.log("Connected to DB"), err =>
-    console.log("Can't connect to DB")
-  );
+MongoClient.connect(
+  process.env.DB,
+  { useNewUrlParser: true },
+  (err, client) => {
+    if (err) return console.log(err);
+    db = client.db("mb-clone01-190219");
+    app.listen(port, () => console.log("Listening on port " + port));
+  }
+);
 
 function log(req, res, next) {
   console.log(new Date() + " -- Server request made.");
@@ -22,7 +24,6 @@ function log(req, res, next) {
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", log, (req, res) => res.sendFile(__dirname + "/index.html"));
-app.listen(port, () => console.log("Listening on port " + port));
 
 app.post("/monsters", (req, res) => {
   db.collection("monsters").save(req.body, (err, result) => {
@@ -31,4 +32,3 @@ app.post("/monsters", (req, res) => {
     res.redirect("/");
   });
 });
-
