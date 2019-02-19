1cc366bd0e84276ff256a0beabb3ff6b1037f797
James Gary <gmarchesi888@gmail.com>
2019-02-19 11:05:49 -0500

    Serves index.html.

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 
+// function log() {
+//   console.log("Server request made.");
+// }
+
+// app.use("/", log);
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+// app.get("/",(req, res) => res.send("Blah blah blah"));
 app.listen(3000, () => console.log("Listening on 3000"));
-
-function log() {
-  console.log("Server request made.");
-}
-
-app.use("/", log);
-app.get("/", (req, res) => res.send("Up and running!!"));
